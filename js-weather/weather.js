3f0b71749e17fdb3e70ffdc03df4a8a6026172a8
nanakusa27 <masakiw27@yahoo.co.jp>
2020-10-08 14:30:40 +0900

    Improve:var -> constに変更,arrow関数に変更

--- js-weather/weather.js
+++ js-weather/weather.js
@@ -1,34 +1,33 @@
-var place = document.getElementById('place');
-
-place.addEventListener('change', function(place) {
-  place = document.getElementById('place');
-  weather(place);
-});
-
-weather(place);
-
-function weather(place) {
-  var appid = '4b5774e9f3d2a07b84f0f2f88e486224';
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + place.value + '&units=metric&lang=ja&APPID=' + appid;
+const weather = (place) => {
+  const appid = '4b5774e9f3d2a07b84f0f2f88e486224';
+  const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + place.value + '&units=metric&lang=ja&APPID=' + appid;
   fetch(requestUrl)
-    .catch(error => console.log(error))
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('description')
-        .textContent = data['weather'][0]['description'];
-      document.getElementById('weather_icon')
-        .src = 'img/' + data['weather'][0]['icon'] + '.png';
-      document.getElementById('temp')
-        .textContent = Math.round(data['main']['temp'] * 10)/ 10 + ' ℃';
-      document.getElementById('pressure')
-        .textContent = data['main']['pressure'] + ' hPa';
-      document.getElementById('humidity')
-        .textContent = data['main']['humidity'] + ' ％';
-      document.getElementById('wind_icon')
-        .src = 'img/wind1.png';
-      document.getElementById('wind_icon')
-        .style.transform = 'rotate(' + data['wind']['deg'] + 'deg)';
-      document.getElementById('wind_speed')
-        .textContent = data['wind']['speed'] + ' m/s';
-    });
+  .catch(error => console.log(error))
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('description')
+    .textContent = data['weather'][0]['description'];
+    document.getElementById('weather_icon')
+    .src = 'img/' + data['weather'][0]['icon'] + '.png';
+    document.getElementById('temp')
+    .textContent = Math.round(data['main']['temp'] * 10)/ 10 + ' ℃';
+    document.getElementById('pressure')
+    .textContent = data['main']['pressure'] + ' hPa';
+    document.getElementById('humidity')
+    .textContent = data['main']['humidity'] + ' ％';
+    document.getElementById('wind_icon')
+    .src = 'img/wind1.png';
+    document.getElementById('wind_icon')
+    .style.transform = 'rotate(' + data['wind']['deg'] + 'deg)';
+    document.getElementById('wind_speed')
+    .textContent = data['wind']['speed'] + ' m/s';
+  });
 }
+
+const defaultPlace = document.getElementById('place');
+weather(defaultPlace);
+
+defaultPlace.addEventListener('change', () => {
+  const changePlace = document.getElementById('place');
+  weather(changePlace);
+});
